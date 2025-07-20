5cda0b2fcdaa72c1e67928a0c84cb9988963ec1b
Sanket6498 <sanket6498@gmail.com>
2025-07-20 15:30:00 +0530

    Dependencies added

---
+++ src/test/java/com/amazon/login_Functionality/AmazonLogin.java
@@ -0,0 +1,10 @@
+package com.amazon.login_Functionality;
+
+import org.testng.annotations.Test;
+
+public class AmazonLogin {
+
+	@Test
+	public void f() {
+	}
+}
---
+++ src/test/java/com/amazon/login_Functionality/AmazonPOM.java
@@ -0,0 +1,5 @@
+package com.amazon.login_Functionality;
+
+public class AmazonPOM {
+
+}
